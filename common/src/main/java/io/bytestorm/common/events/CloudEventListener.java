07e2d33fd081ad28368b4b863ecbf6b0ec4f80cd
KeineSecrets <miethe.max@web.de>
2025-06-11 20:39:51 +0200

    Set default priority to MEDIUM in CloudEventListener annotation
    Signed-off-by: KeineSecrets <miethe.max@web.de>

--- common/src/main/java/io/bytestorm/common/events/CloudEventListener.java
+++ common/src/main/java/io/bytestorm/common/events/CloudEventListener.java
@@ -5,5 +5,5 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface CloudEventListener {
-    CloudEventPriority priority() default 0;
+    CloudEventPriority priority() default CloudEventPriority.MEDIUM;
 }
